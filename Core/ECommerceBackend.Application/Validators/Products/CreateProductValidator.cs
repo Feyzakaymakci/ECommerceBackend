5af2a6c1aee4f25525ef65f548d9c03a2183d1da
feyzakaymakci <feyza.kaymakci@hotmail.com>
2023-03-31 14:03:53 +0300

    31.03.2023

---
+++ Core/ECommerceBackend.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,36 @@
+﻿using ECommerceBackend.Application.ViewModels.Products;
+using FluentValidation;
+
+
+namespace ECommerceBackend.Application.Validators.Products
+{
+    public class CreateProductValidator:AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen ürün adını boş geçmeyiniz.")
+                .MaximumLength(150)
+                .MinimumLength(5)
+                .WithMessage("Lütfen ürün adını 5 ile 150 karakter arasında giriniz");
+
+
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen stok bilgisini boş geçmeyiniz.")
+                .Must(s => s >= 0)
+                .WithMessage("Lütfen stok bilgisini boş geçmeyiniz.");
+
+
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen fiyat bilgisini boş geçmeyiniz.")
+                .Must(s => s >= 0)
+                .WithMessage("Lütfen fiyat bilgisini boş geçmeyiniz.");
+        }
+    }
+}
---
+++ Infrastructure/ECommerceBackend.Infrastructure/Filters/ValidationFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBackend.Infrastructure.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid) //Eğerki geçersizse errorları clienta geri döndürüyoruz.
+            {
+                var errors = context.ModelState
+                    .Where(x => x.Value.Errors.Any())
+                    .ToDictionary(e => e.Key, e => e.Value.Errors.Select(e => e.ErrorMessage)) //Buradaki key bze ilgili property yi getirecek.
+                    .ToArray();  //Diziye dönüştürüyoruz.
+
+                context.Result = new BadRequestObjectResult(errors);
+                return;
+            }
+            await next();
+        }
+    }
+}
+ 
