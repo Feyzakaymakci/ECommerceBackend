02dd2d3d916b4f0190adeae829aca30a9e58d563
Feyza Kaymakci <feyza.kaymakci@hotmail.com>
2023-05-13 13:27:41 +0300

    13.05.2023

---
+++ Core/ECommerceBackend.Application/Features/Queries/GetAllProduct/GetAllProductQueryRequest.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceBackend.Application.RequestParameters;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBackend.Application.Features.Queries.GetAllProduct
+{
+    public class GetAllProductQueryRequest :IRequest<GetAllProductQueryResponse>  //Mediatr sayesinde hangi sınıfın request sınıfı olduğunu ve bu request sınıfın neticesinde hangi sınıfın response olarak geriye döndüreleceğini biliyoruz.
+    {
+        public Pagination Pagination { get; set; }
+    }
+}
